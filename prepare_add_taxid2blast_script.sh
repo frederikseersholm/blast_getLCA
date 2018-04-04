4d6a98083bb764561eb86c88d88e0b9a393713a2
frederikseersholm <frederikseersholm@gmail.com>
2018-04-04 15:24:17 +0800

    Update prepare_add_taxid2blast_script.sh

--- prepare_add_taxid2blast_script.sh
+++ prepare_add_taxid2blast_script.sh
@@ -1,14 +1,14 @@
 #! /bin/bash -x
-#mkdir accession2taxid
-#cd accession2taxid
-#rm nucl*
-#wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl*gz
-#gunzip *gz
-#split -l 50000000 nucl_gb.accession2taxid nucl_gb.accession2taxid.
-#split -l 50000000 nucl_est.accession2taxid nucl_est.accession2taxid.
-#split -l 50000000 nucl_gss.accession2taxid nucl_gss.accession2taxid.
-#split -l 50000000 nucl_wgs.accession2taxid nucl_wgs.accession2taxid.
-#cd ..
+mkdir accession2taxid
+cd accession2taxid
+rm nucl*
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl*gz
+gunzip *gz
+split -l 50000000 nucl_gb.accession2taxid nucl_gb.accession2taxid.
+split -l 50000000 nucl_est.accession2taxid nucl_est.accession2taxid.
+split -l 50000000 nucl_gss.accession2taxid nucl_gss.accession2taxid.
+split -l 50000000 nucl_wgs.accession2taxid nucl_wgs.accession2taxid.
+cd ..
 
 path=$(pwd)"/accession2taxid/gi_taxid_nucl.dmp.*"
 
