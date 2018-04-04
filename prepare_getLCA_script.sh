46e8b3ed5bb502a6aa80f8860a905a94cf5903ea
frederikseersholm <frederikseersholm@gmail.com>
2018-04-04 13:56:42 +0800

    Add files via upload

---
+++ prepare_getLCA_script.sh
@@ -0,0 +1,10 @@
+mkdir taxdump 
+wget ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz 
+tar -xzf taxdump.tar.gz -C taxdump 
+rm taxdump.tar.gz
+
+nodes=$(pwd)"/taxdump/nodes.dmp"
+names=$(pwd)"/taxdump/names.dmp"
+
+sed -i.bak 's|/PATH/to/FILE/taxdump/names.dmp|'$names'|g' blast_getLCA.py
+sed -i.bak 's|/PATH/to/FILE/taxdump/nodes.dmp|'$nodes'|g' blast_getLCA.py
